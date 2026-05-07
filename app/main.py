cb2a717f2a0721be19fb67a2814afda037f48953
VideoKompany <135313844+VlaDOS1408@users.noreply.github.com>
2026-05-08 00:59:02 +0400

    add plug endpoint & define app in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 import asyncio
 
-async def main():
-    print("Hello from fapidemo!")
+from fastapi import FastAPI, Request
+import uvicorn
 
-asyncio.run(main())
+from app.core.config import settings
+
+
+app = FastAPI(
+    debug=settings.DEBUG,
+)
+
+@app.get('/')
+async def home_route(request: Request):
+    i = 0
+    for i in range(2000):
+        pass
+    return {'ok': True, 'msg': f'Банька парилка, кипяток и данилка {i}'}
+
+if __name__ == '__main__':
+    uvicorn.run(app)
