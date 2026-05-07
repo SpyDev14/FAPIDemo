801f9070976e88b7407db4715d3b3473215cafc2
SpyDev14 <135313844+SpyDev14@users.noreply.github.com>
2026-05-07 14:34:18 +0400

    add SECRET_KEY checks: should be specified in production mode

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    SECRET_KEY: str
-    DEBUG: bool
+    DEBUG: bool = False # sould be under SK 'cause SK validation depends on DEBUG value
+    SECRET_KEY: str = 'SECRET_KEY'
 
     DB_USER: str
     DB_PASS: str
@@ -10,5 +10,13 @@ class Settings(BaseSettings):
     DB_NAME: str
 
     model_config = SettingsConfigDict(env_file=".env")
+
+    @field_validator('SECRET_KEY')
+    @classmethod
+    def _validate_secret_key(cls, value: str, info: ValidationInfo):
+        debug = info.data['DEBUG']
+        if not debug and value in {cls.SECRET_KEY, 'SECRET_KEY'}:
+            raise ValueError('SECRET_KEY should be specified in production')
+        return value
 
 settings = Settings()
