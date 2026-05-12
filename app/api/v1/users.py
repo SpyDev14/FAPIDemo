4bd68466b9e3c32adf974602338f38efb1cd4697
SpyDev14 <135313844+SpyDev14@users.noreply.github.com>
2026-05-12 23:44:43 +0400

    add draft user api routes

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.api.dependencies import get_current_user
 from app.modules.user     import User
@@ -7,10 +7,23 @@ from app.modules.user     import User
 router = APIRouter(prefix='users')
 
 # $filter = *{attrs for filtering}
-# GET   /me                                    (req: user)
-# GET   /me/accounts                           (req: user)
-# GET   /me/accounts/{number}                  (req: user)
-# GET   /me/accounts/{number}/payments?$filter (req: user)
+# GET   /me                                (req: user)
+# GET   /me/accounts                       (req: user)
+# GET   /me/accounts/{id}                  (req: user)
+# GET   /me/accounts/{id}/payments?$filter (req: user)
 
 @router.get('/me')
-async def get_me(user: User = ): ...
+async def get_me(user: User = Depends(get_current_user)):
+    raise NotImplementedError
+
+@router.get('/me/accounts')
+async def get_my_accounts_list(user: User = Depends(get_current_user)):
+    raise NotImplementedError
+
+@router.get('/me/accounts/{id}')
+async def get_my_account_detail(account_id: int, user: User = Depends(get_current_user)):
+    raise NotImplementedError
+
+@router.get('/me/accounts/{id}/payments')
+async def get_my_account_payments(account_id: int, user: User = Depends(get_current_user)):
+    raise NotImplementedError
