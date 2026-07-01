c21bcde6d1e9822e8e32ce1e21c3d1c1cda61535
SpyDev14 <135313844+SpyDev14@users.noreply.github.com>
2026-07-01 22:15:14 +0400

    use WebhookResponse instead dict annotation for webhook payment responce_model

--- app/api/v1/webhooks.py
+++ app/api/v1/webhooks.py
@@ -1,14 +1,19 @@
-from typing import Literal
+from pydantic import BaseModel
 from fastapi import APIRouter, Depends
+
 from app.modules.accounts import PaymentWebhookData, AccountService, get_account_service
+
 
 router = APIRouter(prefix='/webhooks', tags=['webhooks'])
 
-@router.post('/payment', response_model=dict[Literal['detail'], str])
+class WebhookResponse(BaseModel):
+    detail: str
+
+@router.post('/payment')
 async def payment(
         data: PaymentWebhookData,
         service: AccountService = Depends(get_account_service),
-    ):
+    ) -> WebhookResponse:
 
     processed = await service.try_process_payment(data)
-    return {'detail': 'Successful processed' if processed else 'Processed already'}
+    return WebhookResponse(detail='Successful processed' if processed else 'Processed already')
